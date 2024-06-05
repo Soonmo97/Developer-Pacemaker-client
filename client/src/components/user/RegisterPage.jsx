fbb5104692b8b97f713ef49407ce4a5e94d5913e
ohjin5 <ok024097@gmail.com>
2024-06-06 02:29:14 +0900

    feat: 로그인 , 회원가입 기능 완성

---
+++ client/src/components/user/RegisterPage.jsx
@@ -0,0 +1,605 @@
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+import LoginPage from "../../pages/LoginPage";
+import axios from "axios";
+
+const responsiveWidth = css`
+  width: 100%;
+  @media (max-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 1024px) {
+    width: 1000px;
+  }
+`;
+const Container1 = styled.div`
+  margin: 0 auto 20px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  ${responsiveWidth}
+`;
+const responsiveContainer = css`
+  width: 100%;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+  @media (min-width: 1024px) {
+    width: 1000px;
+  }
+`;
+
+const Label = styled.label`
+  @media (max-width: 768px) {
+    font-size: 0.8em;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 1em;
+  }
+`;
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const Container = styled.div`
+  ${responsiveContainer}
+  width: 1000px;
+  margin: 0 auto 20px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 1.5em;
+  color: #333;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+const Text = styled.div`
+  font-size: 1em;
+  color: #666;
+  line-height: 1.5;
+  text-align: justify;
+  margin: 0 20px;
+
+  @media (max-width: 768px) {
+    font-size: 0.4em;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 1.2em;
+  }
+`;
+const IdContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const DivId = styled.div`
+  width: 10%;
+  padding: 8px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Inputid = styled.input`
+  width: 50%;
+  padding: 8px;
+  margin-left: 10px;
+`;
+
+const CheckButton = styled.button`
+  padding: 8px 10px;
+  cursor: pointer;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+
+  @media (max-width: 768px) {
+    padding: 6px 8px;
+    font-size: 0.8em;
+  }
+
+  @media (min-width: 1024px) {
+    padding: 10px 12px;
+    font-size: 1em;
+  }
+`;
+const PwContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const DivPw = styled.div`
+  width: 10%;
+  padding: 8px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Inputpw = styled.input`
+  width: 50%;
+  padding: 8px;
+  margin-left: 10px;
+`;
+const Title = styled.h1`
+  text-align: center;
+  padding-top: 10px;
+  padding-right: 20px;
+  padding-bottom: 50px;
+  padding-left: 40px;
+`;
+
+const NmContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const DivNm = styled.div`
+  width: 10%;
+  padding: 8px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Inputnm = styled.input`
+  width: 50%;
+  padding: 8px;
+  margin-left: 10px;
+`;
+
+const EmContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const DivEm = styled.div`
+  width: 10%;
+  padding: 8px;
+  display: flex;
+  justify-content: flex-end;
+`;
+const Inputem = styled.input`
+  width: 50%;
+  padding: 8px;
+  margin-left: 10px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 20px;
+`;
+
+const ErrorSpan = styled.span`
+  color: red;
+  margin: 5px;
+`;
+
+const Span = styled.span`
+  margin: 5px;
+`;
+
+// function RegisterPage() {
+//   const [formData, setFormData] = useState({
+//     id: "",
+//     pw: "",
+//     nickname: "",
+//     email: "",
+//   });
+//   const [errors, setErrors] = useState({});
+//   const [serverError, setServerError] = useState("");
+//   const [showRegister, setShowRegister] = useState(true);
+//   const [isRegistered, setIsRegistered] = useState(false);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [availabilityMessages, setAvailabilityMessages] = useState({});
+//   const [validations, setValidations] = useState({
+//     id: null,
+//     nickname: null,
+//     email: null,
+//   });
+
+//   const handleChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+//     if (e.target.name === "id") {
+//       const validChars = /^[a-zA-Z0-9]+$/;
+//       if (!validChars.test(e.target.value) && e.target.value !== "") {
+//         setErrors({
+//           ...errors,
+//           [e.target.name]: "ID는 영어와 숫자만 입력 가능합니다.",
+//         });
+//         setAvailabilityMessages({
+//           ...availabilityMessages,
+//           [e.target.name]: (
+//             <ErrorSpan>ID는 영어와 숫자만 입력 가능합니다.</ErrorSpan>
+//           ),
+//         });
+//         return;
+//       }
+//       if (e.target.value.length < 5) {
+//         setErrors({ ...errors, [e.target.name]: "5글자 이상 입력해주세요" });
+//         setAvailabilityMessages({
+//           ...availabilityMessages,
+//           [e.target.name]: <ErrorSpan>5글자 이상 입력해주세요</ErrorSpan>,
+//         });
+//       } else {
+//         setErrors({ ...errors, [e.target.name]: "" });
+//         setAvailabilityMessages({
+//           ...availabilityMessages,
+//           [e.target.name]: "",
+//         });
+//       }
+//     }
+//     if (e.target.name === "pw") {
+//       if (e.target.value.length < 5) {
+//         setErrors({ ...errors, [e.target.name]: "5글자 이상 입력해주세요" });
+//         setAvailabilityMessages({
+//           ...availabilityMessages,
+//           [e.target.name]: <ErrorSpan>5글자 이상 입력해주세요</ErrorSpan>,
+//         });
+//       } else {
+//         setErrors({ ...errors, [e.target.name]: "" });
+//         setAvailabilityMessages({
+//           ...availabilityMessages,
+//           [e.target.name]: "",
+//         });
+//       }
+//     }
+//   };
+
+//   const handleBack = () => {
+//     setShowRegister(false);
+//   };
+
+//   //   const checkDuplicate = async (field) => {
+//   //     if (!formData[field]) {
+//   //       setErrors({ ...errors, [field]: "This field cannot be empty." });
+//   //       setAvailabilityMessages({ ...availabilityMessages, [field]: "" });
+//   //       return;
+//   //     }
+//   //     try {
+//   //       const response = await axiosUtils.post(`/users/check-duplicate`, {
+//   //         field: field,
+//   //         value: formData[field],
+//   //       });
+//   //       if (response.data === false) {
+//   //         setErrors({ ...errors, [field]: "이미 사용중 입니다." });
+//   //         setAvailabilityMessages({
+//   //           ...availabilityMessages,
+//   //           [field]: <ErrorSpan>이미 사용중입니다.</ErrorSpan>,
+//   //         });
+//   //         setValidations({ ...validations, [field]: false });
+//   //       } else {
+//   //         setErrors({ ...errors, [field]: "" });
+//   //         setAvailabilityMessages({
+//   //           ...availabilityMessages,
+//   //           [field]: <Span>사용 가능합니다.</Span>,
+//   //         });
+//   //         setValidations({ ...validations, [field]: true });
+//   //       }
+//   //     } catch (error) {
+//   //       console.error("Failed to check duplicate:", error);
+//   //       setErrors({ ...errors, [field]: "Failed to check. Please try again." });
+//   //       setAvailabilityMessages({ ...availabilityMessages, [field]: "" });
+//   //     }
+//   //   };
+
+//   if (!showRegister) {
+//     return <LoginPage />;
+//   }
+
+//   if (isRegistered) {
+//     return <LoginPage />;
+//   }
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     if (Object.values(errors).some((error) => error)) {
+//       console.log(Object.values(errors));
+//       console.log(Object.values(errors).some((error) => error));
+//       alert("Please resolve the errors before submitting.");
+//       return;
+//     }
+
+//     if (
+//       Object.values(validations).includes(null) ||
+//       Object.values(validations).includes(false)
+//     ) {
+//       alert("제출 전 모든 유효성 검사를 진행해주세요");
+//       return;
+//     }
+
+//     // try {
+//     //   const response = await axiosUtils.post(`/users/signup`, formData);
+//     //   if (response.data === true) {
+//     //     alert("회원가입이 성공적으로 완료 되었습니다.");
+//     //     setIsRegistered(true);
+//     //   } else if (response.data.errors) {
+//     //   } else {
+//     //     setServerError("오류가 발생했습니다. 다시 실행해주세요.");
+//     //   }
+//     // } catch (error) {
+//     //   console.error("Registration error:", error);
+//     //   setServerError("서버 에러 발생");
+//     // }
+//   };
+//   return (
+//     <MainContainer>
+//       <Container>
+//         <Title>회원가입을 위한 절차</Title>
+//         <SubTitle>회원가입 주의사항</SubTitle>
+//         <Text>
+//           <div>회원가입 시 다음 사항을 유의해 주세요:</div>
+//           <ul>
+//             <li>모든 입력 필드는 반드시 작성해야 합니다.</li>
+//             <li>
+//               이메일 주소는 회원가입 확인 및 비밀번호 재설정에 사용됩니다.
+//             </li>
+//             <li>닉네임은 다른 사용자와 중복될 수 없습니다.</li>
+//             <li>
+//               아이디와 비밀번호는 5글자 이상 입력해주세요. 또한 아이디는 12글자
+//               이하, 비밀번호는 10글자 이하로 설정해주세요.{" "}
+//             </li>
+//             <li>닉네임은 3글자 이상 입력해주세요.</li>
+//             <li>이메일은 형식을 지켜서 작성해주시기 바립니다.</li>
+//           </ul>
+//         </Text>
+//       </Container>
+//       <Container>
+//         <Title>회원가입</Title>
+//         <form onSubmit={handleSubmit}>
+//   <EmContainer>
+//     <DivEm>
+//       <Label htmlFor="email">Email:</Label>
+//     </DivEm>
+//     <Inputem
+//       type="email"
+//       id="email"
+//       name="email"
+//       value={formData.email}
+//       onChange={handleChange}
+//       required
+//     />
+//     <CheckButton type="button">Check</CheckButton>
+//     {/* <CheckButton type="button" onClick={() => checkDuplicate("email")}>
+//       Check
+//     </CheckButton> */}
+//     <span>{availabilityMessages.email}</span>
+//   </EmContainer>
+//           <NmContainer>
+//             <DivNm>
+//               <Label htmlFor="nickname">Nickname:</Label>
+//             </DivNm>
+//             <Inputnm
+//               type="text"
+//               id="nickname"
+//               name="nickname"
+//               value={formData.nickname}
+//               onChange={handleChange}
+//               required
+//             />
+//             <CheckButton type="button">Check</CheckButton>
+//             {/* <CheckButton
+//               type="button"
+//               onClick={() => checkDuplicate("nickname")}
+//             >
+//               Check
+//             </CheckButton> */}
+//             <span>{availabilityMessages.nickname}</span>
+//           </NmContainer>
+//   <PwContainer>
+//     <DivPw>
+//       <Label htmlFor="pw">Password:</Label>
+//     </DivPw>
+//     <Inputpw
+//       type="password"
+//       id="pw"
+//       name="pw"
+//       value={formData.pw}
+//       onChange={handleChange}
+//       required
+//       maxLength={10}
+//     />
+//     <span>{availabilityMessages.pw}</span>
+//   </PwContainer>
+
+//           <ButtonContainer>
+//             <CheckButton type="submit">Sign Up</CheckButton>
+//             <CheckButton type="button" onClick={handleBack}>
+//               돌아가기
+//             </CheckButton>
+//           </ButtonContainer>
+//         </form>
+//       </Container>
+//     </MainContainer>
+//   );
+// }
+
+// export default RegisterPage;
+
+function RegisterPage() {
+  const [formData, setFormData] = useState({
+    nickname: "",
+    email: "",
+    pw: "",
+  });
+  const [serverError, setServerError] = useState("");
+  const [errors, setErrors] = useState({});
+  const [showRegister, setShowRegister] = useState(true);
+  const [isRegistered, setIsRegistered] = useState(false);
+  const [availabilityMessages, setAvailabilityMessages] = useState({
+    nickname: "",
+    email: "",
+  });
+
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+
+    if (name === "nickname" || name === "email") {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/user/check-${name}?${name}=${value}`
+        );
+        const isDuplicate = response.data; // 서버에서 반환한 중복 여부 값 사용
+
+        console.log("aaaaa", isDuplicate);
+
+        if (isDuplicate) {
+          if (name === "nickname") {
+            setAvailabilityMessages({
+              ...availabilityMessages,
+              [name]: `이미 사용 중인 닉네임 입니다.`,
+            });
+          } else {
+            setAvailabilityMessages({
+              ...availabilityMessages,
+              [name]: `이미 사용 중인 이메일 입니다.`,
+            });
+          }
+        } else {
+          if (name === "nickname") {
+            setAvailabilityMessages({
+              ...availabilityMessages,
+              [name]: `사용가능한 닉네임 입니다.`,
+            });
+          } else {
+            setAvailabilityMessages({
+              ...availabilityMessages,
+              [name]: `사용가능한 이메일 입니다.`,
+            });
+          }
+        }
+      } catch (error) {
+        console.error(`Error checking ${name} availability:`, error);
+      }
+    }
+  };
+
+  if (!showRegister) {
+    return <LoginPage />;
+  }
+
+  const handleBack = () => {
+    setShowRegister(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/user",
+        formData
+      );
+
+      if (response.data === true) {
+        alert("회원가입이 성공적으로 완료 되었습니다.");
+        setIsRegistered(true);
+      } else if (response.data.errors) {
+      } else {
+        setServerError("오류가 발생했습니다. 다시 실행해주세요.");
+      }
+    } catch (error) {
+      console.error("Registration error:", error);
+    }
+  };
+
+  return (
+    <MainContainer>
+      <Container1>
+        <Title>회원가입을 위한 절차</Title>
+        <SubTitle>회원가입 주의사항</SubTitle>
+        <Text>
+          <div>회원가입 시 다음 사항을 유의해 주세요:</div>
+          <ul>
+            <li>모든 입력 필드는 반드시 작성해야 합니다.</li>
+            <li>
+              이메일 주소는 회원가입 확인 및 비밀번호 재설정에 사용됩니다.
+            </li>
+            <li>반드시 이메일 형태로 입력해주세요.</li>
+            <li>닉네임은 3글자 이상 입력해주세요.</li>
+            <li>이메일은 형식을 지켜서 작성해주시기 바립니다.</li>
+          </ul>
+        </Text>
+      </Container1>
+      <Container>
+        {/* 양식 내용 */}
+        <form onSubmit={handleSubmit}>
+          {/* 입력 필드 */}
+          <EmContainer>
+            <DivEm>
+              <Label htmlFor="email">Email:</Label>
+            </DivEm>
+            <Inputem
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <span>{availabilityMessages.email}</span>
+          </EmContainer>
+          <NmContainer>
+            <DivNm>
+              <Label htmlFor="nickname">Nickname:</Label>
+            </DivNm>
+            <Inputnm
+              type="text"
+              id="nickname"
+              name="nickname"
+              value={formData.nickname}
+              onChange={handleChange}
+              required
+            />
+            {/* 유효성 검사 오류 메시지 */}
+            <span>{availabilityMessages.nickname}</span>
+          </NmContainer>
+          <PwContainer>
+            <DivPw>
+              <Label htmlFor="pw">Password:</Label>
+            </DivPw>
+            <Inputpw
+              type="password"
+              id="pw"
+              name="pw"
+              value={formData.pw}
+              onChange={handleChange}
+              required
+              maxLength={10}
+            />
+            <span>{errors.pw}</span>
+          </PwContainer>
+
+          <ButtonContainer>
+            <CheckButton type="submit">Sign Up</CheckButton>
+            <CheckButton type="button" onClick={handleBack}>
+              돌아가기
+            </CheckButton>
+          </ButtonContainer>
+        </form>
+      </Container>
+    </MainContainer>
+  );
+}
+
+export default RegisterPage;
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,259 @@
+import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { login } from "../../store/modules/login";
+// import RegisterForm from "./RegisterForm";
+// import { useNavigate } from "react-router-dom";
+// import axiosUtils from "../../utils/axiosUtils";
+import styled, { css } from "styled-components";
+import RegisterPage from "../components/user/RegisterPage";
+
+import axios from "axios";
+
+const responsiveWidth = css`
+  width: 100%;
+  @media (max-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 1024px) {
+    width: 1000px;
+  }
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+`;
+
+const Container1 = styled.div`
+  margin: 0 auto 20px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  ${responsiveWidth}
+`;
+const Container2 = styled.div`
+  margin: 0 auto 20px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  ${responsiveWidth}
+`;
+
+const SubTitle = styled.h2`
+  font-size: 1.5em;
+  color: #333;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+const Text = styled.div`
+  font-size: 1em;
+  color: #666;
+  line-height: 1.5;
+  text-align: justify;
+  margin: 0 20px;
+
+  @media (max-width: 768px) {
+    font-size: 0.8em;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 1.2em;
+  }
+`;
+const IdContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const DivId = styled.div`
+  width: 50%;
+  padding: 8px;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const Inputid = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-left: 10px;
+`;
+
+const PwContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const DivPw = styled.div`
+  width: 50%;
+  padding: 8px;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const Inputpw = styled.input`
+  width: 100%;
+  padding: 8px;
+  margin-left: 10px;
+`;
+const Title = styled.h1`
+  text-align: center;
+  padding-top: 10px;
+  padding-right: 20px;
+  padding-bottom: 50px;
+  padding-left: 40px;
+`;
+
+const CheckButton = styled.button`
+  padding: 8px 10px;
+
+  margin-left: 5px;
+  cursor: pointer;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin-top: 20px;
+`;
+
+function LoginPage() {
+  const [showRegister, setShowRegister] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({
+    inputEmail: "",
+    inputPw: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/user/login",
+        {
+          email: formData.inputEmail, // formData에서 inputEmail을 가져와서 전송
+          pw: formData.inputPw, // 비밀번호는 그대로 사용
+        },
+        { headers: { "Content-Type": "application/json" } }
+      );
+      if (response.data) {
+        // 로그인 성공 후 처리
+        setShowModal(true);
+        console.log(response.data);
+      } else {
+        // 로그인 실패 후 처리
+        const errorMessage = response.data.message
+          ? response.data.message.message
+          : "로그인 실패";
+        alert(errorMessage);
+      }
+      // 로그인 성공 후 처리
+    } catch (error) {
+      console.error("Login error:", error);
+      alert("로그인 오류가 발생했습니다. 자세한 내용은 콘솔을 확인해주세요.");
+    }
+  };
+
+  const handleSignup = () => {
+    setShowRegister(true);
+  };
+
+  const closeModalAndNavigate = () => {
+    setShowModal(false);
+  };
+
+  if (showRegister) {
+    return <RegisterPage />;
+  }
+
+  return (
+    <MainContainer>
+      <Container1>
+        <Title>로그인을 위한 절차</Title>
+        <SubTitle>로그인 주의사항</SubTitle>
+        <Text>
+          <div>로그인 시 다음 사항을 유의해 주세요:</div>
+          <ul>
+            <li>모든 입력 필드는 반드시 작성해야 합니다.</li>
+            <li>이메일 형식으로 로그인해주셔야 합니다.</li>
+            <li>처음 이용하시는 분은 아래 회원가입을 통해 진행해주세요.</li>
+          </ul>
+        </Text>
+      </Container1>
+      <Container2>
+        <form onSubmit={handleSubmit}>
+          <Title>로그인</Title>
+          <IdContainer>
+            <DivId>
+              <label>이메일:</label>
+            </DivId>
+            <Inputid
+              type="text"
+              name="inputEmail"
+              value={formData.inputEmail}
+              onChange={handleChange}
+              required
+            />
+          </IdContainer>
+          <PwContainer>
+            <DivPw>
+              <label>비밀번호:</label>
+            </DivPw>
+            <Inputpw
+              type="password"
+              name="inputPw"
+              value={formData.inputPw}
+              onChange={handleChange}
+              required
+            />
+          </PwContainer>
+          <ButtonContainer>
+            <CheckButton type="submit">로그인</CheckButton>
+            <CheckButton type="button" onClick={handleSignup}>
+              회원가입
+            </CheckButton>
+          </ButtonContainer>
+        </form>
+        {showModal && (
+          <div
+            style={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              backgroundColor: "white",
+              padding: "20px",
+              zIndex: 1000,
+            }}
+          >
+            <p>로그인 성공!</p>
+            <button onClick={closeModalAndNavigate}>확인</button>
+          </div>
+        )}
+      </Container2>
+    </MainContainer>
+  );
+}
+
+export default LoginPage;
