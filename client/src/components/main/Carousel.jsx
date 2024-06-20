57fc5f00688e44385d105363c67e3aaf7047bcd6
soonmo <smkwon97@gmail.com>
2024-06-20 16:38:19 +0900

    fix: Carousel.css import 삭제

--- client/src/components/main/Carousel.jsx
+++ client/src/components/main/Carousel.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './Carousel.css';
 import UserCalendar from '../userplanner/UserCalendar';
 import { ThemeProvider } from 'styled-components';
 import { Link } from 'react-router-dom';
